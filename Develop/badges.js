1a130e7e4e9384a9e0802caed975fc3e32766402
Erin Otterstrom <hapiotter@gmail.com>
2020-05-02 22:11:59 -0600

    initial commit

---
+++ Develop/badges.js
@@ -0,0 +1,7 @@
+//code quality: JavaScript
+[![Language grade: JavaScript](https://img.shields.io/lgtm/grade/javascript/g/emberjs/ember.js.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/emberjs/ember.js/context:javascript)
+[![Language grade: JavaScript](https://img.shields.io/lgtm/grade/javascript/g/emberjs/ember.js.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/emberjs/ember.js/context:javascript)
+
+//code quality: Java
+[![Language grade: Java](https://img.shields.io/lgtm/grade/java/g/hibernate/hibernate-orm.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/hibernate/hibernate-orm/context:java)
+[![Language grade: Java](https://img.shields.io/lgtm/grade/java/g/hibernate/hibernate-orm.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/hibernate/hibernate-orm/context:java)
---
+++ Develop/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username",
+    message: "Enter you GitHub repo name:",
+    repo: "repo"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+"package-name": "git+https://<github_token>@github.com/<user>/<repo>.git"
+
+
+init();
